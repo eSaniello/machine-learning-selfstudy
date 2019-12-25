9b0e862b9241b0b1c1ad06a7f685fc63fbc5e53c
eSaniello <shaniel29samadhan@gmail.com>
2019-12-25 15:43:32 -0300

    some fixes

--- Neural Networks/Text classification (movie reviews)/movie_reviews_classifier.py
+++ Neural Networks/Text classification (movie reviews)/movie_reviews_classifier.py
@@ -42,7 +42,4 @@ results = model.evaluate(test_data.batch(512), verbose=2)
 
 # print metrics
 for name, value in zip(model.metrics_names, results):
-  print("%s: %.3f" % (name, value))
-
-
-
+    print("%s: %.3f" % (name, value))
